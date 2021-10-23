07e3aedbca29963af297ffbaf35254ad61b0ca1d
Peddaraju <peddarajum15@gmail.com>
2021-10-22 21:07:37 -0700

    Add files via upload

---
+++ bezkoder/app-tutorial-service/src/main/java/com/app/tutorial/AppTutorialServiceApplication.java
@@ -0,0 +1,13 @@
+package com.app.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTutorialServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppTutorialServiceApplication.class, args);
+	}
+
+}
