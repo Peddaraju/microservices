abbd4372489dbad8d0f24ec1cce88efcdee35510
Peddaraju <peddarajum15@gmail.com>
2021-11-07 07:12:38 -0800

    Add files via upload

---
+++ callicoder/poll/polling-app-service/src/main/java/com/polls/PollingAppServiceApplication.java
@@ -0,0 +1,13 @@
+package com.polls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollingAppServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollingAppServiceApplication.class, args);
+	}
+
+}
