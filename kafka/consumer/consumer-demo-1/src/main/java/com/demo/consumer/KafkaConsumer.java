e71fb2ac0f9a1596402da97fcb901ceec5ed6df4
Pedda Raju <peddarajum15@gmail.com>
2023-05-14 22:37:47 -0500

    commits

--- kafka/consumer/consumer-demo-1/src/main/java/com/demo/consumer/KafkaConsumer.java
+++ kafka/consumer/consumer-demo-1/src/main/java/com/demo/consumer/KafkaConsumer.java
@@ -1,5 +1,6 @@
 package com.demo.consumer;
 
+import com.demo.beans.Greeting;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -12,6 +13,11 @@ public class KafkaConsumer {
     @KafkaListener(topics = "${spring.demo.kafka.topic1}", groupId ="${spring.demo.kafka.groupId}", containerFactory = "kafkaListenerContainerFactory")
     public void consume(String message){
         LOGGER.info(String.format("Message received -> %s", message));
+    }
+
+    @KafkaListener(topics = "${spring.demo.kafka.topic2}", groupId ="${spring.demo.kafka.groupId}", containerFactory = "greetingKafkaListenerContainerFactory")
+    public void consumeGreeting(Greeting greeting){
+        LOGGER.info(String.format("Greeting received -> %s", greeting.getName()));
     }
 
 }
--- kafka/consumer/consumer-demo-1/src/main/java/com/demo/consumer/config/KafkaConsumerConfig.java
+++ kafka/consumer/consumer-demo-1/src/main/java/com/demo/consumer/config/KafkaConsumerConfig.java
@@ -1,5 +1,6 @@
 package com.demo.consumer.config;
 
+import com.demo.beans.Greeting;
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -10,6 +11,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -33,8 +35,8 @@ public class KafkaConsumerConfig {
     @Value("${spring.demo.kafka.security.protocol}")
     private String securityProtocol;
 
-    @Bean
-    public ConsumerFactory<String, String> consumerFactory() {
+
+    private Map<String, Object> getProperties() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
@@ -42,15 +44,29 @@ public class KafkaConsumerConfig {
         props.put("sasl.jaas.config", saslJaasConfig);
         props.put(AdminClientConfig.SECURITY_PROTOCOL_CONFIG, securityProtocol);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        return new DefaultKafkaConsumerFactory<>(props);
+        return props;
+    }
+
+
+    private ConsumerFactory<String, String> consumerFactory(StringDeserializer keyDeserializer, StringDeserializer valueDeserializer) {
+        return new DefaultKafkaConsumerFactory<>(getProperties(), keyDeserializer, valueDeserializer);
+    }
+
+    private ConsumerFactory<String, Greeting> consumerFactory(StringDeserializer keyDeserializer, JsonDeserializer valueDeserializer) {
+        return new DefaultKafkaConsumerFactory<>(getProperties(), keyDeserializer, valueDeserializer);
     }
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory());
+        factory.setConsumerFactory(consumerFactory(new StringDeserializer(), new StringDeserializer()));
+        return factory;
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Greeting> greetingKafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Greeting> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory(new StringDeserializer(), new JsonDeserializer(Greeting.class)));
         return factory;
     }
 }
